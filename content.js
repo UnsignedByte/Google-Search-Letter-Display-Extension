93856750d896397789b71e03e67612de9a3ec190
Maydoh1239 <edlam1889@gmail.com>
2017-12-16 16:50:28 -0800

    add unknown char handle

--- content.js
+++ content.js
@@ -1,16 +1,22 @@
 // content.js
-if ( window.location.hostname == "www.google.com" ){
+try{
+  isImg = document.getElementsByClassName("hdtb-mitem hdtb-msel hdtb-imb")[0].innerHTML=="Images";
+}catch (e){
+  isImg = false;
+}
+if ( window.location.hostname == "www.google.com" && !isImg){
   var input = document.getElementById("lst-ib");
 }else if(window.location.hostname == "stackoverflow.com"){
   var input = document.getElementsByClassName("f-input js-search-field")[0];
 }else{
   var input = null;
 }
-var randDeg = Math.random()*2*Math.PI;
+var indice = 0;
 var isDel = false;
-var oldX = [],oldY = [];
+var oldX = [],oldY = [], oldDeg = [];
 oldX.push(Math.random() * 90 + 5);
 oldY.push(Math.random() * 90 + 5);
+oldDeg.push(Math.random()*2*Math.PI);
 var size = {
   width: window.innerWidth || document.body.clientWidth,
   height: window.innerHeight || document.body.clientHeight
@@ -25,61 +31,54 @@ for(var i = 0; i<input.value.length; i++){
 }
 
 input.oninput = function(){
-  if(!isDel){
-    addText(input.value.length-1);
+  var toDelete = document.getElementsByClassName("LETTER_____ClASS");
+  while (toDelete[0]) {
+    document.body.removeChild(toDelete[0]);
   }
+  addText();
 }
 
-function addText(id){
-  var div = document.createElement("DIV");
-  div.style.backgroundImage = "url(https://raw.githubusercontent.com/UnsignedByte/NewTab-Chrome-Extension/master/letters/"+input.value.charCodeAt(id)+".png)";
-  div.className = "LETTER_____ClASS";
-  div.style.width = img.width+"px";
-  div.style.height = img.height+"px";
-  div.style.backgroundSize = "contain";
-  div.style.backgroundRepeat = "no-repeat";
-  div.style.position = "fixed";
-  div.style.top = oldY[oldY.length-1] + "%";
-  div.style.left = oldX[oldX.length-1] + "%";
-  div.style.transform = "translate(-50%, -50%) rotate(" + randDeg/Math.PI/2*360 + "deg)";
-  div.style.zIndex = "1";
-  document.body.appendChild(div);
-  randDeg+=(Math.random()-0.5)*Math.PI/4;
-  var newX = oldX[oldX.length-1]+100*sizemult*Math.cos(randDeg)/size.width;
-  var newY = oldY[oldY.length-1]+100*sizemult*Math.sin(randDeg)/size.height;
-  if ( newX>95 ){
-    newX -= 90;
-  }else if(newX<5) {
-    newX = 90-newX;
-  }
-  if ( newY>95 ){
-    newY -= 90;
-  }else if(newY<5) {
-    newY = 90-newY;
-  }
-  oldX.push(newX);
-  oldY.push(newY);
+function isASCII(i) {
+    return /^[\x00-\x7F]*$/.test(input.value.charAt(i));
 }
 
-input.onkeydown = function(evt) {
-    evt = evt || window.event;
-    var keyCode = evt.keyCode;
-    var deleteKey = (keyCode == 46), backspaceKey = (keyCode == 8);
-    var sel, deletedText, val;
-    if (deleteKey || backspaceKey) {
-        val = this.value;
-        sel = window.getSelection().toString();
-        if (sel.length) {
-            deletedText = val.slice(sel.start, sel.end);
-        } else {
-            deletedText = val.charAt(deleteKey ? sel.start : sel.start - 1);
-        }
-        for (var i = 0; i<deletedText.length; i++){
-          var toDelete = document.getElementsByClassName("LETTER_____ClASS");
-          document.body.removeChild(toDelete[toDelete.length-1]);
-          var T_ = oldX.pop();
-          var T_ = oldY.pop();
-        }
+function addText(){
+  while(input.value.length>=oldX.length){
+    var randDeg = oldDeg[indice]+(Math.random()-0.5)*Math.PI/4;
+    var newX = oldX[oldX.length-1]+100*sizemult*Math.cos(randDeg)/size.width;
+    var newY = oldY[oldY.length-1]+100*sizemult*Math.sin(randDeg)/size.height;
+    if ( newX>95 ){
+      newX -= 90;
+    }else if(newX<5) {
+      newX = 90-newX;
     }
-    isDel = deleteKey || backspaceKey;
-};
+    if ( newY>95 ){
+      newY -= 90;
+    }else if(newY<5) {
+      newY = 90-newY;
+    }
+    oldX.push(newX);
+    oldY.push(newY);
+    oldDeg.push(randDeg);
+  }
+  for(var i = 0; i<input.value.length; i++){
+    var div = document.createElement("DIV");
+    var request = new XMLHttpRequest();
+    if(isASCII(i)){
+      div.style.backgroundImage = "url(https://raw.githubusercontent.com/UnsignedByte/NewTab-Chrome-Extension/master/letters/"+input.value.charCodeAt(i)+".png)";
+    }else{
+      div.style.backgroundImage = "url(https://raw.githubusercontent.com/UnsignedByte/NewTab-Chrome-Extension/master/letters/13.png)"
+    }
+    div.className = "LETTER_____ClASS";
+    div.style.width = img.width+"px";
+    div.style.height = img.height+"px";
+    div.style.backgroundSize = "contain";
+    div.style.backgroundRepeat = "no-repeat";
+    div.style.position = "fixed";
+    div.style.top = oldY[i] + "%";
+    div.style.left = oldX[i] + "%";
+    div.style.transform = "translate(-50%, -50%) rotate(" + oldDeg[i]/Math.PI/2*360 + "deg)";
+    div.style.zIndex = "1";
+    document.body.appendChild(div);
+  }
+}
